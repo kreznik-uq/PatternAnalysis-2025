a0a959a8548154ea120a016917f7242f92126298
kreznik-uq <k.reznik@student.uq.edu.au>
2025-10-17 02:13:30 +1000

    Implemented validation loop and best model saving based on Dice score.

--- recognition/Segmentation_Prostate_3D_UNet/train.py
+++ recognition/Segmentation_Prostate_3D_UNet/train.py
@@ -1,23 +1,77 @@
 import torch.optim as optim
 import torch
+import tqdm
 import torch.nn as nn
 from modules import UNet3D
 from torch.utils.data import DataLoader
 from dataset import ProstateDataset
 from torch.amp import GradScaler, autocast
 
+# Code reference: https://medium.com/data-scientists-diary/
+# implementation-of-dice-loss-vision-pytorch-7eef1e438f68
+
+def dice_coefficient(pred, target):
+    pred_probs = torch.sigmoid(pred)
+    pred_mask = (pred_probs > 0.5).float()
+
+    intersection = (pred_mask * target).sum()
+    union = pred_mask.sum() + target.sum()
+    
+    dice = (2. * intersection) / (union + 1e-6)
+    return dice.item()
+
+# Code reference: https://medium.com/@fernandopalominocobo/
+# mastering-u-net-a-step-by-step-guide-to-segmentation-from-scratch-with-pytorch-6a17c5916114
+
+def validate(model, loader, criterion, device):
+    model.eval()
+    val_running_loss = 0
+    val_running_dc = 0
+    
+    with torch.no_grad():
+        for inputs, labels in tqdm.tqdm(loader, desc="Validating"):
+            inputs = inputs.float().to(device)
+            labels = labels.float().to(device)
+
+            if labels.max() > 1.0:
+                labels = labels / 255.0
+
+            with autocast(device_type="cuda"):
+                y_pred = model(inputs)
+                loss = criterion(y_pred, labels)
+            
+            dc = dice_coefficient(y_pred, labels)
+            
+            val_running_loss += loss.item()
+            val_running_dc += dc
+
+    avg_val_loss = val_running_loss / len(loader)
+    avg_val_dc = val_running_dc / len(loader)
+    
+    return avg_val_loss, avg_val_dc
 
 if __name__ == '__main__':
 
     dataset = ProstateDataset(image_dir="processed_data/images", label_dir="processed_data/labels")
-    dataloader = DataLoader(dataset, batch_size=1, shuffle=True, num_workers=2)
+
+    dataset_size = len(dataset)
+    val_size = int(dataset_size * 0.2)
+    train_size = dataset_size - val_size
+    train_dataset, val_dataset = torch.utils.data.random_split(dataset, [train_size, val_size])
+
+    dataloader = DataLoader(train_dataset, batch_size=1, shuffle=True, num_workers=2)
+    validationloader = DataLoader(val_dataset, batch_size=1, shuffle=False, num_workers=2)
 
     # Code reference: https://www.codegenes.net/blog/3d-unet-pytorch/
     model = UNet3D(in_channels=1, out_channels=1)
     criterion = nn.BCEWithLogitsLoss()
-    optimizer = optim.Adam(model.parameters())
+    optimizer = optim.Adam(model.parameters(), lr=1e-4)
+    scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'max', patience=5, factor=0.1)
 
-    num_epochs = 2
+    num_epochs = 20
+    best_val_dice = 0.0
+    model_save_path = "best_model.pth"
+
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model.to(device)
 
@@ -25,8 +79,10 @@ if __name__ == '__main__':
 
     print("started")
     for epoch in range(num_epochs):
+        model.train()
         running_loss = 0.0
-        for inputs, labels in dataloader:
+
+        for inputs, labels in tqdm.tqdm(dataloader):
             inputs, labels = inputs.to(device), labels.to(device)
             
             labels = labels.float() 
@@ -50,5 +106,15 @@ if __name__ == '__main__':
 
             running_loss += loss.item()
             print(f'Epoch {epoch + 1}/{num_epochs}, Step Loss: {loss.item()}')
+
+        #Epoch is over, time to validate
+        avg_train_loss = running_loss / len(dataloader)
+        avg_val_loss, avg_val_dice = validate(model, validationloader, criterion, device)
+        scheduler.step(avg_val_dice)
+
+        if avg_val_dice > best_val_dice:
+            best_val_dice = avg_val_dice
+            torch.save(model.state_dict(), model_save_path)
+            print(f"New best model. + {best_val_dice}")
 
         print(f'Epoch {epoch + 1}/{num_epochs}, Loss: {running_loss / len(dataloader)}')
